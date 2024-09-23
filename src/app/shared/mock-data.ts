9d4b7d48c3a75153985e5311934da45f0876df1d
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-09-23 20:01:00 +0300

    добавил phrases.service.ts

---
+++ src/app/shared/mock-data.ts
@@ -0,0 +1,13 @@
+import {Phrase} from './phrase';
+
+export const PHRASES: Phrase[] = [
+  new Phrase(1, 'Привіт Світ', 'Ukrainian'),
+  new Phrase(2, 'Hello world', 'English'),
+  new Phrase(3, 'Hola Mundo', 'Spain'),
+  new Phrase(4, 'Bonjour monde', 'French'),
+  new Phrase(5, 'Hallo Welt', 'German'),
+  new Phrase(6, 'Ciao mondo', 'Italian'),
+  new Phrase(7, 'Witaj świecie', 'Polish'),
+  new Phrase(8, 'Hej världen', 'Swedish'),
+  new Phrase(9, 'Pozdravljen', 'Slovenian'),
+];
