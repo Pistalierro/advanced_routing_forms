87e7ad2526d291da5ad81bb8c9eb5f2b56cc71f9
Anatoliy Sysoienko <pistalierro@gmail.com>
2024-09-23 20:09:35 +0300

    написал логику в phrases-list.component.ts

--- src/app/phrases/phrases-list/phrases-list.component.ts
+++ src/app/phrases/phrases-list/phrases-list.component.ts
@@ -1,15 +1,40 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router, RouterOutlet} from '@angular/router';
+import {Phrase} from '../../shared/phrase';
+import {PhrasesService} from '../../shared/phrases.service';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-phrases-list',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NgFor
   ],
   templateUrl: './phrases-list.component.html',
   styleUrl: './phrases-list.component.scss'
 })
-export class PhrasesListComponent {
+export class PhrasesListComponent implements OnInit {
 
+  phrases!: Phrase[];
+  selectedID!: number;
+
+  constructor(private phrasesService: PhrasesService,
+              private router: Router,
+              private activatedRoute: ActivatedRoute,) {
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe({
+      next: params => {
+        this.selectedID = +params['id'];
+
+        this.phrasesService.getAllPhrases().then(phrases => this.phrases = phrases);
+      },
+    });
+  }
+
+  onSelect(phrase: Phrase) {
+    this.router.navigate([phrase.id], {relativeTo: this.activatedRoute}).then();
+  }
 }
